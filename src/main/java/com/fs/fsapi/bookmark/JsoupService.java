0831d7992f1b77c9eb5b303b6a4217e53637963e
vilsuo <vilsuo1@gmail.com>
2024-08-30 17:33:57 +0300

    moved element type comparison to enum, improved dt element type checking and added comments

--- src/main/java/com/fs/fsapi/bookmark/JsoupService.java
+++ src/main/java/com/fs/fsapi/bookmark/JsoupService.java
@@ -12,31 +12,16 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+// TODO
+// - remove Link class
+// - name FolderLink to Link
+// 
+// - create custom exception class, with more info about exception (folder location etc.)
+
 @Service
 public class JsoupService {
 
-  /*
-  private enum Tag {
-    LINK ("a"),
-    DT ("dt"),
-    DL ("dl");
-
-    private final String name;
-
-    Tag(String name) {
-      this.name = name;
-    }
-  };
-  */
-
-  private final String LINK_TAG = "a";
-  
-  private final String DT_TAG = "dt";
-
-  private final String DL_TAG = "dl";
-
   private final Pattern HEADER_PATTERN = Pattern.compile("h[1-6]");
-
 
   public List<FolderLink> createFolderLinks(MultipartFile file, String headerText) throws IOException {
     Document doc = Jsoup.parse(file.getInputStream(), null, "");
@@ -44,6 +29,14 @@ public class JsoupService {
     return parseFolder(doc, findHeader(doc, headerText), new ArrayList<>());
   }
 
+  /**
+   * Find a header element with the given text content.
+   * 
+   * @param doc Document to search in
+   * @param headerText text content of the header
+   * @return the header element
+   * @throws RuntimeException unless a single such header is found
+   */
   private Element findHeader(Document doc, String headerText) {
     Elements headers = doc.select(
       ":is(h1, h2, h3, h4, h5, h6):matches(^" + headerText + "$)"
@@ -62,7 +55,7 @@ public class JsoupService {
   private List<FolderLink> parseFolder(Document doc, Element h, List<FolderLink> folderLinks) {
     String text = h.text();
 
-    // find the <dl> element following the header
+    // find the dl element following the header
     Element dlElement = h.nextElementSibling();
 
     if (dlElement == null) {
@@ -70,16 +63,16 @@ public class JsoupService {
         "Header '" + text + "' does not have sibling elements"
       );
 
-    } else if (!dlElement.tagName().equals(DL_TAG)) {
+    } else if (!elementHasTag(dlElement, Tag.DL)) {
       throw new RuntimeException(
-        "The next element of header '" + text + "' was not a '" + DL_TAG + "' element"
+        "The next element of header '" + text + "' was not a '" + Tag.DL + "' element"
       );
     }
 
     dlElement.children().stream()
-      .filter(child -> child.tagName().equals(DT_TAG))
+      .skip(1) // first element is paragraph, so skip it
       .forEach(dt -> {
-        if (isSingleDtElement(dt)) {
+        if (isLinkDtElement(dt)) {
           Element link = dt.child(0);
 
           folderLinks.add(
@@ -98,81 +91,95 @@ public class JsoupService {
           parseFolder(doc, findHeader(doc, newText), folderLinks);
 
         } else {
-          throw new RuntimeException("Found invalid '" + DT_TAG + "' element");
+          throw new RuntimeException(
+            "Found unexpected child element type of '" + Tag.DL + "' element"
+          );
         }
       });
 
     return folderLinks;
   }
 
-  private boolean isSingleDtElement(Element dt) {
-    return (dt.childrenSize() == 1) && dt.child(0).tagName().equals(LINK_TAG);
+  private boolean elementHasTag(Element e, Tag tag) {
+    return tag.name.equals(e.normalName());
   }
 
-  private boolean isFolderDtElement(Element dt) {
-    return (dt.childrenSize() > 0)
-      && HEADER_PATTERN.matcher(dt.child(0).tagName()).matches();
+  /**
+   * Check if an element is a {@link Tag#DT} element with the structure
+   * {@code
+   *  <dt>
+   *    <a />
+   *  </dt>
+   * }.
+   * 
+   * @param e the element to check
+   * @return true if element is a certain {@link Tag#DT} element
+   */
+  private boolean isLinkDtElement(Element e) {
+    return elementHasTag(e, Tag.DT)
+      && (e.childrenSize() == 1)
+      && elementHasTag(e.child(0), Tag.LINK);
   }
+
+  /**
+   * Check if element is a {@link Tag#DT} element with the structure
+   * {@code
+   *  <dt>
+   *    <h? />
+   *    <dl />
+   *    <p />
+   *  </dt>
+   * }. 
+   * 
+   * @implNote Does not check the structure of child elements.
+   * 
+   * @param e the element to check
+   * @return true if element is a certain {@link Tag#DT} element
+   */
+  private boolean isFolderDtElement(Element e) {
+    return elementHasTag(e, Tag.DT)
+      && (e.childrenSize() == 3)
+      && HEADER_PATTERN.matcher(e.child(0).normalName()).matches()
+      && elementHasTag(e.child(1), Tag.DL)
+      && elementHasTag(e.child(2), Tag.P);
+  }
+
+  private enum Tag {
+    P ("p"),
+    LINK ("a"),
+
+    /**
+     * HTML-element with the tag {@code dt}. The child structure is
+     * one of the following
+     * 
+     * <ol>
+     *  <li>Just a single html link element {@code <a />}.</li>
+     *  <li>
+     *    Exactly three elements in the order {@code <h? /> <dl /> <p />}.
+     *    The tag {@code h?} is a html header element of any level and
+     *    {@code dl} has structure specified by {@link Tag#DL}. The header
+     *    indicates the beginning of a subfolder structure and the {@code dl}
+     *    element the contents of that folder.
+     *  </li>
+     * </ol>
+     * 
+     * @see {@link JsoupService#isLinkDtElement}
+     * @see {@link JsoupService#isFolderDtElement}
+     */
+    DT ("dt"),
+
+    /**
+     * HTML-element with the tag {@code dl}. The child structure is
+     * {@code <p /> <dt /> ... <dt />}, where each {@code dt} is a 
+     * structure specified by {@link Tag#DT}.
+     */
+    DL ("dl");
+
+    private final String name;
+
+    Tag(String name) {
+      this.name = name;
+    }
+  };
   
-  /*
-  @Getter
-  public abstract class Element<T> {
-
-    private final String tagName;
-
-    private T content;
-
-    public Element(String tagName, T content) {
-      this.tagName = tagName;
-      this.content = content;
-    }
-  }
-
-  @Getter
-  public class HeaderElement extends Element<String> {
-
-    public HeaderElement(int level, String content) {
-      super("h" + level, content);
-    }
-  }
-
-  @Getter
-  public class LinkElement extends Element<String> {
-
-    private final String href;
-
-    private final String addDate;
-
-    public LinkElement(String content, String href, String addDate) {
-      super("a", content);
-
-      this.href = href;
-      this.addDate = addDate;
-    }
-  }
-
-  @Getter
-  public class DtSingleElement extends Element<LinkElement> {
-    
-    public DtSingleElement(LinkElement link) {
-      super("dt", link);
-    }
-  };
-
-  @Getter
-  public class DtFolderElement extends Element<List<LinkElement>> {
-
-    public DtFolderElement(List<LinkElement> links) {
-      super("dt", links);
-    }
-  };
-
-  @Getter
-  public class DlElement extends Element<List<LinkElement>> {
-
-    public DlElement(List<LinkElement> links) {
-      super("dl", links);
-    }
-  };
-  */
 }

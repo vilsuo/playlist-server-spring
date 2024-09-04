da4a0689c6c141ff1484e9865e86f3a5cef7ba78
vilsuo <vilsuo1@gmail.com>
2024-09-04 13:16:55 +0300

    added tests

--- src/test/java/com/fs/fsapi/parser/HtmlParserServiceTest.java
+++ src/test/java/com/fs/fsapi/parser/HtmlParserServiceTest.java
@@ -1,6 +1,8 @@
 package com.fs.fsapi.parser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,25 +17,49 @@ import org.junit.jupiter.api.Test;
 
 import com.fs.fsapi.bookmark.parser.FolderLink;
 import com.fs.fsapi.bookmark.parser.HtmlParserService;
+import com.fs.fsapi.exceptions.CustomParameterConstraintException;
 
 // TODO test files with
 // - invalid format
-// - empty folder
 public class HtmlParserServiceTest {
 
   private final HtmlParserService service = new HtmlParserService();
 
   private final String TEST_FILES_LOCATION = "src/test/data";
 
-  private final String EXAMPLE = "bookmarks-example.html";
+  private final String FILE_NAME = "bookmarks-example.html";
+
+  private final String PARENT = "Example";
+  private final String CHILD = "Sub";
+  private final String EMPTY = "Empty";
   
   private InputStream getFileAsInputStream(String filename) throws FileNotFoundException {
     File initialFile = new File(TEST_FILES_LOCATION + "/" + filename);
     return new FileInputStream(initialFile);
   }
 
+  @Test
+  public void shouldThrowWhenFolderDoesNotWithTheTextContentTest() throws Exception {
+    final String nonExisting = "Punk";
+
+    CustomParameterConstraintException ex = assertThrows(
+      CustomParameterConstraintException.class, 
+      () -> service.createFolderLinks(getFileAsInputStream(FILE_NAME), nonExisting)
+    );
+
+    assertEquals(
+      "Could not find a header with the text content '" + nonExisting + "'",
+      ex.getMessage()
+    );
+  }
+
+  @Test
+  public void shouldNotCreateAnyWhenFolderIsEmptyTest() throws Exception {
+    assertTrue(service.createFolderLinks(getFileAsInputStream(FILE_NAME), EMPTY).isEmpty());
+  }
+
   @Nested
-  @DisplayName("folder with single album")
+  @DisplayName("folder with a single album")
   public class Sub {
 
     private List<FolderLink> results;
@@ -41,7 +67,7 @@ public class HtmlParserServiceTest {
 
     @BeforeEach
     public void create() throws Exception {
-      results = service.createFolderLinks(getFileAsInputStream(EXAMPLE), "Sub");
+      results = service.createFolderLinks(getFileAsInputStream(FILE_NAME), CHILD);
       result = results.get(0);
     }
 
@@ -52,17 +78,23 @@ public class HtmlParserServiceTest {
 
     @Test
     public void shouldGetFolderNameFromHeaderTextContentTest() {
-      assertEquals("Sub", result.getFolderName());
+      assertEquals(CHILD, result.getFolderName());
     }
 
     @Test
     public void shouldGetTextFromLinkTextContentTest() {
-      assertEquals("Angel Dust - Into the Dark Past (1986)", result.getText());
+      assertEquals(
+        "Angel Dust - Into the Dark Past (1986)",
+        result.getText()
+      );
     }
 
     @Test
     public void shouldGetHrefFromLinkHrefAttributeTest() {
-      assertEquals("https://www.youtube.com/watch?v=DopHEl-BCGQ", result.getHref());
+      assertEquals(
+        "https://www.youtube.com/watch?v=DopHEl-BCGQ",
+        result.getHref()
+      );
     }
     
     @Test
@@ -72,14 +104,14 @@ public class HtmlParserServiceTest {
   }
 
   @Nested
-  @DisplayName("folder with a sub album")
+  @DisplayName("folder with a sub folder")
   public class WithSub {
 
     private List<FolderLink> results;
 
     @BeforeEach
     public void create() throws Exception {
-      results = service.createFolderLinks(getFileAsInputStream(EXAMPLE), "Example");
+      results = service.createFolderLinks(getFileAsInputStream(FILE_NAME), PARENT);
     }
 
     @Test
@@ -87,8 +119,21 @@ public class HtmlParserServiceTest {
       assertEquals(5, results.size());
     }
 
-    // link folderName before "Sub" are "Example"
-    // link folderName after "Sub" are "Example"
-    // link folderName in "Sub" are "Sub"
+    @Test
+    public void shouldParentFolderNameForLinksBeforeSubFolderTest() {
+      assertEquals(PARENT, results.get(0).getFolderName());
+      assertEquals(PARENT, results.get(1).getFolderName());
+      assertEquals(PARENT, results.get(2).getFolderName());
+    }
+
+    @Test
+    public void shouldSetSubFolderNameForLinksInSubFolderTest() {
+      assertEquals(CHILD, results.get(3).getFolderName());
+    }
+
+    @Test
+    public void shouldParentFolderNameForLinksAfterSubFolderTest() {
+      assertEquals(PARENT, results.get(4).getFolderName());
+    }
   }
 }

b1c336b0a9e49aab607aabf778d76c20f5457813
vilsuo <vilsuo1@gmail.com>
2024-09-11 15:28:09 +0300

    specified unmapped properties in albummapper, removed unnecessary methods from album repository

--- src/main/java/com/fs/fsapi/album/AlbumMapper.java
+++ src/main/java/com/fs/fsapi/album/AlbumMapper.java
@@ -3,10 +3,14 @@ package com.fs.fsapi.album;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
 import com.fs.fsapi.bookmark.parser.AlbumBase;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+  componentModel = MappingConstants.ComponentModel.SPRING,
+  unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface AlbumMapper {
   
   public Album albumCreationToAlbum(AlbumCreation source);
--- src/main/java/com/fs/fsapi/album/AlbumRepository.java
+++ src/main/java/com/fs/fsapi/album/AlbumRepository.java
@@ -1,16 +1,10 @@
 package com.fs.fsapi.album;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
-  
-  Optional<Album> findById(Integer id);
-
-  void deleteById(Integer id);
 
   // https://docs.spring.io/spring-data/commons/reference/repositories/query-methods-details.html
   // https://docs.spring.io/spring-data/commons/reference/repositories/query-keywords-reference.html#appendix.query.method.subject

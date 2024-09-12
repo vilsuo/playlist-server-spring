bbd43a7d61d3cde59fd7fbdeb82c21a423c1e417
vilsuo <vilsuo1@gmail.com>
2024-09-12 13:48:25 +0300

    tested song lyrics parser

--- src/test/java/com/fs/fsapi/metallum/parser/MetallumParserTest.java
+++ src/test/java/com/fs/fsapi/metallum/parser/MetallumParserTest.java
@@ -1,10 +1,13 @@
 package com.fs.fsapi.metallum.parser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -71,5 +74,95 @@ public class MetallumParserTest {
   @DisplayName("parseLyrics")
   public class ParseLyrics {
 
+    @Nested
+    @DisplayName("when song lyrics are available")
+    public class Available {
+
+      final LyricsResult expected = MetallumFileHelper.lyrics;
+      LyricsResult actual;
+
+      @BeforeEach
+      public void parse() throws IOException {
+        actual = parser.parseLyrics(MetallumFileHelper.readLyricsFile());
+      }
+
+      @Test
+      public void shouldNotHaveErrorTest() throws IOException {
+        assertTrue(actual.getError().isEmpty());
+      }
+
+      @Test
+      public void shouldHaveTheSongLyricsTest() throws IOException {
+        List<String> expectedLyrics = expected.getLyrics();
+        List<String> actualLyrics = actual.getLyrics();
+  
+        assertEquals(expectedLyrics.size(), actualLyrics.size());
+        for (int i = 0; i < actual.getLyrics().size(); ++i) {
+          assertEquals(expectedLyrics.get(i), actualLyrics.get(i));
+        }
+      }
+    }
+
+    @Nested
+    @DisplayName("when song lyrics are not available")
+    public class NotAvailable {
+
+      final LyricsResult expected = MetallumFileHelper.lyricsNotAvailable;
+      LyricsResult actual;
+
+      @BeforeEach
+      public void parse() throws IOException {
+        actual = parser.parseLyrics(
+          MetallumFileHelper.readLyricsNotAvailableFile()
+        );
+      }
+
+      @Test
+      public void shouldHaveErrorTest() throws IOException {
+        final String error = actual.getError();
+        assertFalse(error.isEmpty());
+        assertEquals("Lyrics not available", error);
+      }
+
+      @Test
+      public void shouldNotHaveAnySongLyricsTest() throws IOException {
+        List<String> expectedLyrics = expected.getLyrics();
+        List<String> actualLyrics = actual.getLyrics();
+  
+        assertTrue(expectedLyrics.isEmpty());
+        assertTrue(actualLyrics.isEmpty());
+      }
+    }
+
+    @Nested
+    @DisplayName("when song is instrumental")
+    public class Instrumental {
+
+      final LyricsResult expected = MetallumFileHelper.lyricsInstrumental;
+      LyricsResult actual;
+
+      @BeforeEach
+      public void parse() throws IOException {
+        actual = parser.parseLyrics(
+          MetallumFileHelper.readLyricsInstrumentalFile()
+        );
+      }
+
+      @Test
+      public void shouldHaveErrorTest() throws IOException {
+        final String error = actual.getError();
+        assertFalse(error.isEmpty());
+        assertEquals("Instrumental", error);
+      }
+
+      @Test
+      public void shouldNotHaveAnySongLyricsTest() throws IOException {
+        List<String> expectedLyrics = expected.getLyrics();
+        List<String> actualLyrics = actual.getLyrics();
+  
+        assertTrue(expectedLyrics.isEmpty());
+        assertTrue(actualLyrics.isEmpty());
+      }
+    }
   }
 }
